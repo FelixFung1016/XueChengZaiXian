ae5e3c0072612fae493935b2f4bc5cf57f335b3a
fyb <fyb@qq.com>
2020-01-19 22:32:27 +0800

    Create CmsPage query Controller,Service,Repository. Add Swagger annotation.

---
+++ xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/CmsPageRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.xuecheng.manage_cms;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CmsPageRepositoryTest {
+
+    @Autowired
+    CmsPageRepository cmsPageRepository;
+
+    @Test
+    public void testFindPage(){
+        Pageable pageable = PageRequest.of(0,10);
+        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        System.out.println(all);
+
+    }
+}
